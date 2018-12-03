deeb58ca1b1989d6a2f1249f82c3abe2a110f3f9
Mat Silva <askmatsilva@gmail.com>
2018-12-02 22:23:02 -0500

    Add reload window on dock icon click

--- main/main.js
+++ main/main.js
@@ -14,6 +14,10 @@ function createWindow() {
     if (isDevMode) {
         win.webContents.openDevTools();
     }
+
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
 app.on('ready', createWindow);
@@ -30,7 +34,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
+    if (win === null) {
         createWindow();
     }
 });
